84e5067697cd6c58592816eaf4c9bfaeb583f332
arozovyk <artemiy.rozovyk@ocamlpro.com>
2022-02-03 18:14:04 +0100

    npm i -D react-error-overlay@6.0.9

--- frontend/src/DisplayMarketItems.js
+++ frontend/src/DisplayMarketItems.js
@@ -4,38 +4,7 @@ import Carousel from "react-grid-carousel";
 
 import { getTokenUri, fetchMarketItems, weiToEther } from "./Web3Client";
 
-const IMAGES = [
-  {
-    src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-    thumbnail:
-      "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 174,
-    isSelected: true,
-    caption: "After Rain (Jeshu John - designerspics.com)",
-  },
-  {
-    src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-    thumbnail:
-      "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 212,
-    tags: [
-      { value: "Ocean", title: "Ocean" },
-      { value: "People", title: "People" },
-    ],
-    caption: "Boats (Jeshu John - designerspics.com)",
-  },
-
-  {
-    src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-    thumbnail:
-      "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-    thumbnailWidth: 320,
-    thumbnailHeight: 212,
-    thumbnailCaption: "test",
-  },
-];
+ 
 
 export class DisplayMarketItems extends React.Component {
   constructor(props) {
@@ -114,7 +83,7 @@ export class DisplayMarketItems extends React.Component {
       <Panel header="NFTs on sale:" bordered>
         <div>
           {this.state.marketItems != null ? (
-            <Carousel cols={2} rows={1} gap={3} loop>
+            <Carousel cols={4} rows={1} gap={0} loop>
               
               {this.state.marketItems.map((nft, i) => (
                 <Carousel.Item key={i}>
